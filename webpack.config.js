cda0cfb9f932db5749a24f602a0acad98269588f
ronilson-user <ronilson.stos@gmail.com>
2023-11-11 10:32:38 -0300

    src e afinco

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,54 @@ const { exec } = require('child_process');
 const path = require('path');
 
 module.exports = (env, options) => {
-  const { mode = 'development' } = options;
-  const rules = [
+ const { mode = 'development' } = options;
+ const rules = [
+  {
+   test: /\.m?js$/,
+   use: [
+    'html-tag-js/jsx/tag-loader.js',
     {
-      test: /\.m?js$/,
-      use: [
-        'html-tag-js/jsx/tag-loader.js',
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      ],
+     loader: 'babel-loader',
+     options: {
+      presets: ['@babel/preset-env'],
+     },
     },
-  ];
+   ],
+  },
+ ];
 
-  const main = {
-    mode,
-    entry: {
-      main: './src/main.js',
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js',
-      chunkFilename: '[name].js',
-    },
-    module: {
-      rules,
-    },
-    plugins: [
-      {
-        apply: (compiler) => {
-          compiler.hooks.afterDone.tap('pack-zip', () => {
-            // run pack-zip.js
-            exec('node .acode/pack-zip.js', (err, stdout, stderr) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-              console.log(stdout);
-            });
-          });
+ const main = (options) => {
+  target: 'node'
+  mode: options.mode || 'development'
+  entry: {
+   main: './src/main.js'
+  }
+  output: {
+   path: path.resolve(__dirname, 'dist')
+   filename: '[name].js'
+   chunkFilename: '[name].js'
+  }
+  module: {
+   rules
+  }
+  plugins: [
+   {
+    apply: (compiler) => {
+     compiler.hooks.afterDone.tap('pack-zip', () => {
+      // run pack-zip.js
+      exec('node .acode/pack-zip.js,
+      (err, stdout, stderr)=> {
+  if (err) {
+   console.error(err);
+   return;
+  }
+  console.log(stdout);
+ });
+});
         }
       }
     ],
   };
 
-  return [main];
+return [main];
 }
