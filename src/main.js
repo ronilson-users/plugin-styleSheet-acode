7dfeca23dc8d1629c080a9b94be4ad72935bd691
ronilson-users <ronilson.stos@gmail.com>
2023-12-27 12:07:06 -0300

    atual

--- src/main.js
+++ src/main.js
@@ -1,60 +1,120 @@
-import plugin from "../plugin.json";
+// main.js
+import plugin from '../plugin.json';
 import LRUCache from './cache.js';
+import { reactNativeStyles } from './properties/Properties.js';
+import { snippets } from './snippets/Snippets.js'
 
-
-const fsOperation = acode.require("fsOperation");
-const { editor } = editorManager;
-
+const { activeFile, editor } = editorManager;
+const { snippetManager } = ace.require('ace/snippets');
 const fileList = acode.require('fileList');
-
 const list = await fileList();
 
-list.forEach((item) => {
-  console.log(item.root);
-});
+// Extração de informações do arquivo atual
+const FILENAME = activeFile.filename; // Obtém o nome do arquivo atual
+const FILENAME_BASE = FILENAME.substring(0, FILENAME.lastIndexOf('.')); // Obtém o nome do arquivo sem extensão
+const FILEPATH = activeFile.uri; // Obtém o caminho completo do arquivo
+const DIRECTORY = FILEPATH ? FILEPATH.substring(FILEPATH.indexOf('::/') + 3, FILEPATH.lastIndexOf('/')) : '';
+// Extrai o diretório do arquivo atual do caminho completo
 
 
-class AcodeAutoImport {
+// Classe AcodePluginSnippets para gerenciar os snippets no editor
+
+class AcodePluginSnippets {
   constructor() {
-    this.diretorioCache = new LRUCache(50);
-    this.baseUrl = ''; // Defina a baseURL se necessário
+    this.directoryCache = new LRUCache();
+    this.editor = editor;
+    this.snippetManager = snippetManager;
+    this.initializeSnippetInsertion();
+    this.importIntellisense();
+  }
+
+  // Inicializa a inserção de snippets no editor
+  initializeSnippetInsertion() {
+    this.editor.completers = [this];
   }
 
 
+  // ImportIntellisense inicial
+  importIntellisense() {
+    list.forEach(item => {
+      // Adicionando os caminhos ao LRUCache
+      this.directoryCache.set(item.name, {
+        url: item.url,
+        path: item.path,
+      });
+    });
 
 
-  async init($page, { cacheFileUrl, cacheFile }) {
-    if (!this.baseUrl.endsWith('/')) {
-      this.baseUrl += '/';
+
+    console.log('LRUCache', path)
+    // Adicionar event listener para capturar a entrada do usuário
+    this.editor.on('input', () => this.searchComponent(this.editor.session));
+  } // fim intellisense
+
+  // Obtém sugestões de completar ( snippets ou styles )
+  getCompletions(editor, session, pos, prefix, callback) {
+    const cursor = editor.getCursorPosition();
+    const line = session.getLine(cursor.row);
+    const lastWord = line.substr(0, cursor.column).split(/\s+/).pop();
+
+    // Procura por snippets que começam com a última palavra digitada
+    const matchedSnippets = snippets.filter(snippet => snippet.prefix.startsWith(lastWord));
+
+    if (matchedSnippets.length > 0 && matchedSnippets[0].prefix !== lastWord) {
+      // Se há snippets correspondentes, retorna sugestões de snippets
+
+      const suggestions = matchedSnippets.map(snippet => ({
+        caption: snippet.prefix,
+        value: snippet.snippet,
+        meta: 'snippet',
+        score: 600,
+      }));
+
+      callback(null, suggestions);
+    } else {
+      // Se não houver snippets correspondentes, busca por estilos de React Native
+
+      const matchedStyles = Object.keys(reactNativeStyles).filter(style => style.startsWith(lastWord));
+
+      if (matchedStyles.length > 0 && matchedStyles[0] !== lastWord) {
+        // Se há estilos correspondentes, retorna sugestões de estilos de React Native
+
+        const styleSuggestions = matchedStyles.map(style => ({
+          caption: style,
+          value: style,
+          meta: 'Prop.Css RN',
+          score: 600,
+        }));
+
+        callback(null, styleSuggestions);
+      } else {
+        // Se não houver correspondências, retorna um array vazio
+        callback(null, []);
+      }
     }
-    this.diretorioCache.set('cacheFileUrl', cacheFileUrl);
+  } // fim getCompletions
 
-    // Exemplo de console.log para depuração
-    console.log('AcodeAutoImport iniciado.');
-    console.log('this.baseUrl:', this.baseUrl);
-    console.log('cacheFileUrl armazenado na cache:', this.diretorioCache.get('cacheFileUrl'));
-
-    // Outras operações de inicialização do componente
+  async init($page, cacheFile, cacheFileUrl) {
+    console.log(cacheFile, cacheFileUrl);
+    // Implementação da lógica de inicialização, se aplicável
   }
 
+  // Lógica de destruição, se necessário
   async destroy() {
-    // Operações de destruição do componente
-    this.diretorioCache.resetCache();
-
-    // Exemplo de console.log para depuração
-    console.log('AcodeAutoImport destruído. Cache limpa.');
+    // Implementação da lógica de destruição, se aplicável
   }
 }
 
-// Verifica se o objeto 'acode' existe no contexto atual (provavelmente o ambiente do Acode)
+// Instância do plugin e inicialização
 if (window.acode) {
-  const acodePlugin = new AcodeAutoImport();
-
+  const acodePlugin = new AcodePluginSnippets();
   acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
+    if (!baseUrl.endsWith('/')) {
+      baseUrl += '/';
+    }
     acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, { cacheFileUrl, cacheFile });
+    await acodePlugin.init($page, cacheFile, cacheFileUrl);
   });
-
   acode.setPluginUnmount(plugin.id, () => {
     acodePlugin.destroy();
   });
